f88f040e45a521682e099000493712c0403168e1
vjalankar <33005492+vjalankar@users.noreply.github.com>
2022-01-06 17:55:50 +0530

    first commit

---
+++ src/Components/NewsItem.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl,newsUrl } = this.props;
+
+    return (
+      <div>
+        <div className="card bg-dark text-light text-justify shadow rounded p-2" >
+          {console.title}
+
+          <img
+            src={imageUrl}
+            className="card-img-top img-fluid img-responsive vw-100 vh-100" style={{objectFit:"cover"}}
+           
+            alt="can not be displayed at this moment "
+
+           
+          />
+          <hr className="text-warning" />
+          
+          <div className="card-body ">
+            <h5 className="card-titl text-primary">{title}...</h5>
+
+            { <hr className="text-primary"/>
+           }
+            <p className="card-text text-light ">{description}...</p>
+            <hr className="text-light"/>
+
+           <center> <a
+              href={newsUrl} target="_blank" rel="noreferrer"
+              className="btn btn-outline-primary "
+            >
+              Read Full News
+            </a>
+            </center>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
